d0a73b068915243af18b123341b1503441f71ead
hjfrun <hjfrun@gmail.com>
2024-04-13 22:31:43 +0800

    init a server

--- index.ts
+++ index.ts
@@ -1,1 +1,8 @@
-console.log('Hello via Bun!')
+const server = Bun.serve({
+  port: 5000,
+  fetch(req) {
+    return new Response('Hello World!')
+  }
+})
+
+console.log(`Server running at http://localhost:${server.port}`)
